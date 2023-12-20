9f06b78a75c021d68d8beaa37e27632eb46a710b
samuel21695 <samuel21695@gmail.com>
2023-12-20 17:45:54 +0900

    ✨function setupEventListners

--- app.ts
+++ app.ts
@@ -40,3 +40,21 @@ function renderPokemons(pokemons: Pokemon[]): string {
 
   return createElement('div', {}, `배틀 횟수: ${battles}`, pokemonButtonsHtml);
 }
+
+function setupEventListeners(rootId: string, pokemons: Pokemon[]): void {
+  const rootElement = document.getElementById(rootId);
+
+  if (rootElement === null) {
+    return;
+  }
+
+  for (let pokemon of pokemons) {
+    const button = document.getElementById(`pokemon-${pokemon.id}`);
+    
+    if (button === null) {
+      continue;
+    }
+
+    button.addEventListener('click', () => handleBattle(pokemon.name));
+  }
+}
